a86f3d830f470ab31410adae2b433ead37e96206
Edward Osorno <edward-1094@hotmail.com>
2019-12-10 23:53:31 +0000

    add 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_last_number(number):
+def print_last_digit(number):
     if number < 0:
         number = -number
     number = number % 10
-    print("{:d}".format(number))
+    print("{:d}".format(number), end='')
     return number
