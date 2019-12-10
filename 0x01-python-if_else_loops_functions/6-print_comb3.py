0e860fae823aec9b2640001a307ece569d298ee0
Edward Osorno <edward-1094@hotmail.com>
2019-12-10 23:31:47 +0000

    add 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-print("00", end='')
 for i in range(0, 10):
     for j in range(i + 1, 10):
-        print(", {:d}{:d}".format(i, j), end='')
-print("")
+        if i is not 8:
+            print("{:d}{:d}, ".format(i, j), end='')
+print("89")
