8142f9b22eb089316a30668a2f0d57e4af7c95f9
Edward Osorno <edward-1094@hotmail.com>
2019-12-11 16:49:56 +0000

    add 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - search for a loop
+ * @list: linked list
+ * Return: 0 no cycle 1 cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	tmp = list;
+	while (tmp->next)
+	{
+		tmp2 = list;
+		while (tmp2 != tmp->next && tmp2->next)
+		{
+			if (tmp->next == tmp2)
+				return (1);
+			tmp2 = tmp2->next;
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
