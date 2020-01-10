c40665022549548cebdd9ce452ece67e394ab468
Edward Osorno <edward-1094@hotmail.com>
2020-01-10 18:53:45 +0000

    add

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    a = 0
+    for i in range(0,x):
+        try:
+            a = a + 1
+            print("{:d}".format(my_list[i]), end='')
+        except:
+            a = a - 1
+    print()
+    return a
