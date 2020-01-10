d05e383ae2631ef76e6d66c1a768dde7ff941e41
Edward Osorno <edward-1094@hotmail.com>
2020-01-10 03:46:51 +0000

    add

--- 0x06-python-classes/4-sas.py
+++ 0x06-python-classes/4-sas.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, size=0):
+    def __init__(self, size):
         self.__size = size
+
 
     @property
     def size(self):
@@ -9,12 +10,14 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = value
 
+
+
     def area(self):
-        return self.__size * self.__size
+        return pow(self.__size, 2)
