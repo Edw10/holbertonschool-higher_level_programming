25a029e1d1abeef5de5e1dcc59bba0536b46747c
Edward Osorno <edward-1094@hotmail.com>
2020-01-27 19:04:40 +0000

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,7 @@ from base import Base
 class Rectangle(Base):
     """this is the rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """"""
+        """init a rectangle class"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -19,50 +19,50 @@ class Rectangle(Base):
 
     @property
     def width(self):
-    """return width"""
+        """return width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-    """width setter"""
+        """width setter"""
         self.validator("width", value)
         self.__width = value
 
     @property
     def height(self):
-    """"""
+        """"""
         return self.__height
 
     @height.setter
     def height(self, value):
-    """"""
+        """"""
         self.validator("height", value)
         self.__height = value
 
     @property
     def x(self):
-    """"""
+        """"""
         return self.__x
 
     @x.setter
     def x(self, value):
-    """"""
+        """"""
         self.validator("x", value)
         self.__x = value
 
     @property
     def y(self):
-    """"""
+        """"""
         return self.__y
 
     @y.setter
     def y(self, value):
-    """"""
+        """"""
         self.validator("y", value)
         self.__y = value
 
     def validator(self, name, value):
-    """"""
+        """"""
         if type(value) is not int:
             raise TypeError(name + ' must be an integer')
         if name == 'x' or name == 'y':
@@ -73,22 +73,22 @@ class Rectangle(Base):
                 raise ValueError(name + ' must be > 0')
 
     def area(self):
-    """"""
+        """"""
         return (self.width * self.height)
 
     def display(self):
-    """"""
+        """"""
         print("\n"*self.y, end='')
         print((" "*self.x + "#"*self.width + '\n')*(self.height), end='')
 
     def __str__(self):
-    """"""
+        """"""
         return ('[Rectangle] (' + str(self.id) + ') ' +
                 str(self.x) + '/' + str(self.y) +
                 ' - ' + str(self.width) + '/' + str(self.height))
 
     def update(self, *args, **kwargs):
-    """"""
+        """"""
         if args is not None:
             i = 0
             for args in args:
