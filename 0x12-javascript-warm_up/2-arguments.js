db10e7958e93533e298606b36ac2fa0689814b22
Edward Osorno <edward-1094@hotmail.com>
2020-05-06 05:09:28 +0000

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ let argv = process.argv;
 
 if (argv[2] === undefined){
   console.log('No argument');
-} else if (argv[3] == null) {
+} else if (argv[3] === undefined) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
