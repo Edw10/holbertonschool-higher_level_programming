3fd135a095dae7a342174707439bf4bb8412fe67
Edw10 <edward-1094@hotmail.com>
2020-05-13 04:54:31 +0000

    add

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, res, bd) => {
   if (err) return err;
-  console.log(JSON.parse(bd)['title']);
+  console.log(JSON.parse(bd).title);
 });
