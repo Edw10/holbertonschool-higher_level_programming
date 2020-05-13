344c8d925fbd197720d28794574da096ba54c591
Edw10 <edward-1094@hotmail.com>
2020-05-13 05:13:40 +0000

    add

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,6 +5,7 @@ const request = require('request');
 
 request(process.argv[2], (err, res, bd) => {
   if (err) return err;
-  fs.writeFile(process.argv[3], bd, 'utf8');
-  console.log(JSON.parse(bd).title);
+  fs.writeFile(process.argv[3], bd, 'utf8', (e) => {
+    if (e) return e;
+  });
 });
