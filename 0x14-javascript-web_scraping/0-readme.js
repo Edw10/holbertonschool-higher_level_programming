e5f7d1d22861e4cfea951c1e42e260f8136addc7
Edw10 <edward-1094@hotmail.com>
2020-05-13 04:25:43 +0000

    add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,6 +4,9 @@
 var fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
