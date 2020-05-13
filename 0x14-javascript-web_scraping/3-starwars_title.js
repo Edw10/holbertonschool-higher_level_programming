57594e005b4444e3188929d243cf8ed8edcaac77
Edw10 <edward-1094@hotmail.com>
2020-05-13 04:53:09 +0000

    add

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// this script return the title of the starwars movie
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, res, bd) => {
+  if (err) return err;
+  console.log(JSON.parse(bd)['title']);
+});
