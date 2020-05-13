5a3c75109ac5e6708cd14e02171d08880a0e0172
Edw10 <edward-1094@hotmail.com>
2020-05-13 05:27:58 +0000

    add

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,20 +2,19 @@
 // this script return the completed tasks
 
 const request = require('request');
-let res = Object();
-let rest = Object();
+const rest = Object();
 
 request(process.argv[2], (err, res, bd) => {
   if (err) return err;
-    for (const i in JSON.parse(bd)) {
+  for (const i in JSON.parse(bd)) {
     if (JSON.parse(bd)[i].completed) {
       const id = JSON.parse(bd)[i].userId;
-      if (res.hasOwnProperty(id)) {
+      if (rest.hasOwnProperty(id)) {
         rest[id] += 1;
       } else {
         rest[id] = 1;
       }
     }
   }
-    console.log(rest);
+  console.log(rest);
 });
